51729abda58f730c71ddc70e9126d46d6dedfaef
Ulysses Schultian <schultuj@mail.uc.edu>
2025-09-29 15:36:48 -0400

    Init

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class BirthMonth {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int month = 0;
+        String trash = "";
+
+        System.out.print("Enter your birth month (1–12): ");
+        if (in.hasNextInt()) {
+            month = in.nextInt();
+            in.nextLine();
+
+            if (month >= 1 && month <= 12) {
+                System.out.println("Your birth month is: " + month);
+            } else {
+                System.out.println("You entered an incorrect month value: " + month);
+            }
+        } else {
+            trash = in.nextLine();
+            System.out.println("Invalid input: " + trash);
         }
     }
 }
